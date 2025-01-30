2f98d900157d7fc4e6ce80cbebe89febd11a2710
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2025-01-30 18:53:39 +1000

    Added main python file

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Authors: Harrison, Damon, Daniel, Casey
+CP3407 EXT GROUP Assignment
+This is the main python file
+ENJOY :)
+"""
+
+
+def main():
+# Main function
+    print("Hello world")
+
+if __name__ == '__main__':
+    main()
+
+
